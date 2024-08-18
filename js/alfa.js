a1925a3fea6168dab908bad2ed620404e6c3d648
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-18 12:03:48 +0600

    update function

--- js/alfa.js
+++ js/alfa.js
@@ -1,9 +1,14 @@
-function play(){
-    // add hiden to home screen
-   const home = document.getElementById('home');
-   home.classList.add('hidden')
+// function play(){
+//     // add hiden to home screen
+//    const home = document.getElementById('home');
+//    home.classList.add('hidden')
 
-    //  remove hidden to play ground
-   const playGround = document.getElementById('play-ground');
-   playGround.classList.remove('hidden')
+//     //  remove hidden to play ground
+//    const playGround = document.getElementById('play-ground');
+//    playGround.classList.remove('hidden')
+// }
+
+function play(){
+    hideElementById('home');
+    showelementById('play-ground');
 }
