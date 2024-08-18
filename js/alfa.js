c960b25ef6cc0198cef7ae9d6fe115635cb0f80a
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-18 11:54:46 +0600

    alfa js started

---
+++ js/alfa.js
@@ -0,0 +1,9 @@
+function play(){
+    // add hiden to home screen
+   const home = document.getElementById('home');
+   home.classList.add('hidden')
+
+    //  remove hidden to play ground
+   const playGround = document.getElementById('play-ground');
+   playGround.classList.remove('hidden')
+}
