22d76a0a81ffbe3321427030d7290f145793829a
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-08-19 17:13:00 +0600

    update score

--- js/alfa.js
+++ js/alfa.js
@@ -1,14 +1,66 @@
-// function play(){
-//     // add hiden to home screen
-//    const home = document.getElementById('home');
-//    home.classList.add('hidden')
 
-//     //  remove hidden to play ground
-//    const playGround = document.getElementById('play-ground');
-//    playGround.classList.remove('hidden')
-// }
 
 function play(){
     hideElementById('home');
     showelementById('play-ground');
+    continueGame()
 }
+
+function continueGame(){
+    // generate a random alphabet
+    const alphabet = getaRandomAlphabet();
+
+    // set randomly generated alphabet
+
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabet
+
+    // set backgroung color
+
+    setBackgroundColor(alphabet)
+    
+}
+
+// key board 
+
+document.addEventListener('keyup', handleKeyBoardButtobPress)
+
+function handleKeyBoardButtobPress(event){
+    const playerPressed = event.key;
+    // console.log('player pressed ',playerPressed);
+
+    // get the expected to press
+
+    const currentAlphabet = document.getElementById('current-alphabet')
+    const targetAlphabet = currentAlphabet.innerText;
+    const expectedAlphabet= targetAlphabet.toLowerCase()
+    console.log(playerPressed , expectedAlphabet);
+
+    // checking
+
+    if(playerPressed === expectedAlphabet){
+        console.log('win');
+        // updadte score
+
+        // get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentText);
+        console.log(currentScore);
+
+        // imcrease score
+        const newScore = currentScore+ 1;
+
+        currentScoreElement.innerText =newScore;
+
+
+
+
+        removeBackgroundColor(expectedAlphabet)
+        continueGame()
+    }
+    else{
+        console.log('lose');
+    }
+}
+
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,13 @@
+// function play(){
+//     // add hiden to home screen
+//    const home = document.getElementById('home');
+//    home.classList.add('hidden')
+
+//     //  remove hidden to play ground
+//    const playGround = document.getElementById('play-ground');
+//    playGround.classList.remove('hidden')
+// }
+
 function hideElementById(id){
     const element = document.getElementById(id);
     element.classList.add('hidden')
@@ -7,3 +17,31 @@ function showelementById(id){
     const element = document.getElementById(id)
     element.classList.remove('hidden')
 }
+
+function setBackgroundColor(id){
+    const element = document.getElementById(id);
+    element.classList.add('bg-orange-400');
+}
+
+
+function removeBackgroundColor(id){
+    const element = document.getElementById(id);
+    element.classList.remove('bg-orange-400');
+}
+
+
+function getaRandomAlphabet(){
+    
+    const alphabetsString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets =  alphabetsString.split('');
+    // get a ranfdom index between 1 to 25
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber)
+
+    const alphabet = alphabets[index]
+    return alphabet
+}
+
+
+
